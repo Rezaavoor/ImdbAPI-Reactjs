9abc279420003b06966db249cb7096e25f7c5150
Rezaavoor <reza.avoor@gmail.com>
2019-03-16 10:39:33 +0100

    some small fixes

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ A super simple Reactjs app that uses IMDP api to get data
 
 ## More functionalities will be added soon
 I will try to add all the api's fuctions
-Hooks will be added as well
+Later on Hooks will replace the class beasd components
--- src/App.js
+++ src/App.js
@@ -5,56 +5,62 @@ import InputManager from './Components/InputManager/InputManager';
 import InfoCards from './Components/InfoCards/InfoCards';
 class App extends Component {
   state = ({
-    name:'Captain Marvel',
-    movies:undefined
+    name: 'Captain Marvel',
+    movies: undefined
   })
 
   getData = async () => {
-    if (this.state.name){
-    let movies="";
-    const proxyurl = "https://cors-anywhere.herokuapp.com/";//to bypass the error of fetching(google it!)
-    const imdburl= `https://v2.sg.media-imdb.com/suggests/${this.state.name.toLowerCase().charAt(0)}/${this.state.name}.json`; //ex: https://v2.sg.media-imdb.com/suggests/b/batman.json
-    await fetch(proxyurl + imdburl).then(response => response.text())
-    .then(contents => movies=contents)
-    movies = movies.replace('imdb$'+this.state.name.replace(' ','_')+'(',''); 
-    movies= movies.substring(0,movies.length-1); //outputting a clean Object out of the response
-    try{this.setState({
-      movies: JSON.parse(movies)
-    },()=>{
-      console.clear();
-      console.log(this.state.movies.d[0].l); 
-    })}
-    catch(e){   //DO nothing due to an issue that appears when you delete the whole text by holding backspace
+    if (this.state.name) {
+      let movies = "";
+      const proxyurl = "https://cors-anywhere.herokuapp.com/";//to bypass the error of fetching(google it!)
+      const imdburl = `https://v2.sg.media-imdb.com/suggests/${this.state.name.toLowerCase().charAt(0)}/${this.state.name}.json`; //ex: https://v2.sg.media-imdb.com/suggests/b/batman.json
+      await fetch(proxyurl + imdburl).then(response => response.text())
+        .then(contents => movies = contents)
+      movies = movies.replace('imdb$' + this.state.name.replace(' ', '_') + '(', '');
+      movies = movies.substring(0, movies.length - 1); //outputting a clean Object out of the response
+      try {
+        this.setState({
+          movies: JSON.parse(movies)
+        }, () => {
+          console.clear();
+          console.log(this.state.movies.d[0].l);
+        })
+      }
+      catch (e) {   //DO nothing due to an issue that appears when you delete the whole text by holding backspace
+      }
+
     }
-      
-    } 
-    else{
+    else {
       console.clear();
-      console.log("Please type the name of the movies");  
+      console.log("Please type the name of the movies");
     }
   }
-  componentWillMount(){
+  componentWillMount() {
     this.getData();
   }
 
-  inputChangeHandler=(event,index)=>{
-    const value=event.target.value;
+  inputChangeHandler = (event) => {
+    const value = event.target.value;
     this.setState({
-      name:value
+      name: value
     },
-      ()=>{this.getData()})
-    
+      () => { this.getData() })
+
   }
-  clickHandler=()=>{
+  clickHandler = () => {
     this.getData();
   }
   render() {
     return (
       <div className={styles.App}>
-        <InputManager clicked={()=>this.clickHandler()} inputChanged={(event)=>this.inputChangeHandler(event)} name={this.state.name}></InputManager>
-         {this.state.movies?(
-            <InfoCards movies={this.state.movies}></InfoCards>
-          ):(
+        <InputManager 
+          clicked={() => this.clickHandler()} 
+          inputChanged={(event) => this.inputChangeHandler(event)} 
+          name={this.state.name}>
+        </InputManager>
+        {this.state.movies ? (
+          <InfoCards movies={this.state.movies}></InfoCards>
+        ) : (
             <p>hahaha</p>
           )}
       </div>
