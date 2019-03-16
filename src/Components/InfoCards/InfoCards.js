04c6cf9cda9f60933ff0bd11658dce522878552d
Rezaavoor <reza.avoor@gmail.com>
2019-03-16 09:12:25 +0100

    infoCards works now and search function is added

--- src/Components/InfoCards/InfoCards.js
+++ src/Components/InfoCards/InfoCards.js
@@ -1,8 +1,21 @@
 import React from 'react';
-
+import InfoCard from '../InfoCard/InfoCard'
+import styleClasses from './InfoCards.module.css'
 const InfoCards=(props)=>{
+    let infoCards=null;
+    infoCards=(
+        <div className={styleClasses.main}>
+            {props.movies.d.map((movie)=>{
+                return(
+                    <InfoCard movie={movie}></InfoCard>
+                );
+            })}
+        </div>
+    );
     return(
-        <p>This is InfoCards</p>
+        <div>
+            {infoCards}
+        </div>
     );
 }
 export default InfoCards;
--- src/Components/InputManager/InputManager.js
+++ src/Components/InputManager/InputManager.js
@@ -3,8 +3,8 @@ import styleClasses from './InputManager.module.css';
 const InputManager=(props)=>{
     return(
         <div className={styleClasses.searchDiv}>
-            <input onChange={props.inputChanged} className={styleClasses.searchInput} placeholder={'Movie name...'}></input>
-            <button onClick={props.inputChanged} className={styleClasses.searchButton}>Search</button>
+            <input onChange={props.inputChanged} value={props.name} className={styleClasses.searchInput} placeholder={'Movie name...'}></input>
+            <button onClick={props.clicked} className={styleClasses.searchButton}>Search</button>
         </div>
     );
 }
