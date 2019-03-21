bf764ef0bbf5af1c7fad7a6b327e902ff6e2ea68
Rezaavoor <reza.avoor@gmail.com>
2019-03-21 22:07:00 +0100

    some new features added

--- src/Components/InfoCards/InfoCards.js
+++ src/Components/InfoCards/InfoCards.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import Popup from 'reactjs-popup';
+import MoviePopup from '../MoviePopup/MoviePopup';
 import InfoCard from '../InfoCard/InfoCard'
 import styleClasses from './InfoCards.module.css'
+
+
 const InfoCards = (props) => {
+
   let infoCards = null;
   infoCards = (
     <div className={styleClasses.main}>
       {props.movies.d.map((movie) => {
         return (
-          <InfoCard movie={movie}></InfoCard>
+          <Popup
+            trigger={
+              <div>
+                <InfoCard movie={movie}></InfoCard>
+              </div>}
+              modal
+              closeOnDocumentClick>
+            <MoviePopup movie={movie}></MoviePopup>
+          </Popup>
         );
       })}
     </div>
--- src/Components/InputManager/InputManager.js
+++ src/Components/InputManager/InputManager.js
@@ -3,7 +3,7 @@ import styleClasses from './InputManager.module.css';
 const InputManager=(props)=>{
     return(
         <div className={styleClasses.searchDiv}>
-            <input onChange={props.inputChanged} value={props.name} className={styleClasses.searchInput} placeholder={'Movie name...'}></input>
+            <input ref={input => input && input.focus()} onChange={props.inputChanged} value={props.name} className={styleClasses.searchInput} placeholder={'Movie name...'}></input>
             <button onClick={props.clicked} className={styleClasses.searchButton}>Search</button>
         </div>
     );
