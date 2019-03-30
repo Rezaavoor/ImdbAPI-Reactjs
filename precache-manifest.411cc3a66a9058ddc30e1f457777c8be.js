67d3c7565a99908738be06d2e29a48035b4faf6e
Rezaavoor <reza.avoor@gmail.com>
2019-03-30 08:35:12 +0100

    Updates

---
+++ precache-manifest.411cc3a66a9058ddc30e1f457777c8be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "996365603a6d1d822023",
+    "url": "/ImdbAPI-Reactjs/static/js/runtime~main.b053e06b.js"
+  },
+  {
+    "revision": "fad50a6ec4e512031d23",
+    "url": "/ImdbAPI-Reactjs/static/js/main.dd16df39.chunk.js"
+  },
+  {
+    "revision": "1e338159539dd7f55430",
+    "url": "/ImdbAPI-Reactjs/static/js/2.af6bc467.chunk.js"
+  },
+  {
+    "revision": "fad50a6ec4e512031d23",
+    "url": "/ImdbAPI-Reactjs/static/css/main.c3a4aede.chunk.css"
+  },
+  {
+    "revision": "1e338159539dd7f55430",
+    "url": "/ImdbAPI-Reactjs/static/css/2.a74e7050.chunk.css"
+  },
+  {
+    "revision": "b2f4f32878a388c0704daaaa34c08542",
+    "url": "/ImdbAPI-Reactjs/index.html"
+  }
+];
