6bde7578e3912ded95787f856a1f25fe97be9cef
Rezaavoor <reza.avoor@gmail.com>
2019-03-30 08:27:27 +0100

    Updates

---
+++ precache-manifest.1bc2079a6321e0e100bb8ce6120f0ac9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "098204c5ab435d9420d7",
+    "url": "/imdbapi-reactjs/build/static/js/runtime~main.8977c6de.js"
+  },
+  {
+    "revision": "87c096b81d722e93e364",
+    "url": "/imdbapi-reactjs/build/static/js/main.7f6b1741.chunk.js"
+  },
+  {
+    "revision": "bc4c62e41a54f287562d",
+    "url": "/imdbapi-reactjs/build/static/js/2.0760fe00.chunk.js"
+  },
+  {
+    "revision": "87c096b81d722e93e364",
+    "url": "/imdbapi-reactjs/build/static/css/main.c3a4aede.chunk.css"
+  },
+  {
+    "revision": "bc4c62e41a54f287562d",
+    "url": "/imdbapi-reactjs/build/static/css/2.a74e7050.chunk.css"
+  },
+  {
+    "revision": "957460680abdece5c41e388e4b2cb08d",
+    "url": "/imdbapi-reactjs/build/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/imdbapi-reactjs/precache-manifest.4d76148144f7bafd33132973dee11bd6.js"
+  "/imdbapi-reactjs/build/precache-manifest.1bc2079a6321e0e100bb8ce6120f0ac9.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/imdbapi-reactjs/index.html", {
+workbox.routing.registerNavigationRoute("/imdbapi-reactjs/build/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
