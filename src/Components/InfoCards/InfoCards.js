511634c1e79571b85c9d01625a1a28f40f5666b3
Hosseini Reza <reza.hosseini@edu.nacka.se>
2019-09-08 12:05:02 +0200

    fix map filter bug

--- src/Components/InfoCards/InfoCards.js
+++ src/Components/InfoCards/InfoCards.js
@@ -1,31 +1,30 @@
-import React from 'react';
-import Popup from 'reactjs-popup';
-import MoviePopup from '../MoviePopup/MoviePopup';
-import InfoCard from '../InfoCard/InfoCard';
-import styleClasses from './InfoCards.module.css';
+import React from "react";
+import Popup from "reactjs-popup";
+import MoviePopup from "../MoviePopup/MoviePopup";
+import InfoCard from "../InfoCard/InfoCard";
+import styleClasses from "./InfoCards.module.css";
 
 const InfoCards = props => {
-  let infoCards = null;
-  infoCards = (
+  const moviesList = props.movies.d.map(movie => (
+    <Popup
+      trigger={
+        <div>
+          <InfoCard movie={movie} />
+        </div>
+      }
+      modal
+      closeOnDocumentClick
+      key={movie.id}
+    >
+      <MoviePopup movie={movie} />
+    </Popup>
+  ));
+  const infoCards = (
     <div className={styleClasses.main}>
-      {props.movies.d
-        ? props.movies.d.map(movie => {(
-          <Popup
-            trigger={
-              <div>
-                <InfoCard movie={movie} />
-            </div>
-            }
-            modal
-            closeOnDocumentClick
-            key={movie.id}
-          >
-            <MoviePopup movie={movie} />
-          </Popup>
-        )
-        : null}
+      {props.movies.d ? moviesList : null}
     </div>
   );
+
   return <div>{infoCards}</div>;
 };
 export default InfoCards;
