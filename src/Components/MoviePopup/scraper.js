34d48d56cc6555e7248b88cd7da12db1d9e58694
unknown <reza.avoor@gmail.com>
2019-09-21 11:04:37 +0200

    fetch data from https

--- src/Components/MoviePopup/scraper.js
+++ src/Components/MoviePopup/scraper.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default async function getHTML(id) {
   const apikey = "c43428b2";
-  let url = `http://www.omdbapi.com/?i=${id}&apikey=${apikey}`;
+  let url = `https://www.omdbapi.com/?i=${id}&apikey=${apikey}`;
 
   const { data: data } = await axios.get(url);
 
